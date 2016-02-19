afd312f0d42338f9fa313e418e530129d294082d
liujie <liujie@tech.youyuan.com>
2016-02-19 15:31:22 +0800

    gengxin

--- main.go
+++ main.go
@@ -1,36 +1,37 @@
-package main 
+package main
 
 import "fmt"
 import "github.com/liujieking/wechat"
 import "github.com/liujieking/wechat/entry"
 
-type Echo struct{
+type Echo struct {
 	Name string
 	wechat.Callback
 }
-func NewEcho(name string) *Echo{
+
+func NewEcho(name string) *Echo {
 	return &Echo{name}
 }
-func (e *Echo) MsgText(txt *entry.TextRequest, back chan interface{}){
+func (e *Echo) MsgText(txt *entry.TextRequest, back chan interface{}) {
 	wechat.Info("Echo: MsgText ", txt)
 }
-func (e *Echo) MsgImage(img *entry.ImageRequest, back chan interface{}){
+func (e *Echo) MsgImage(img *entry.ImageRequest, back chan interface{}) {
 	wechat.Info("Echo: MsgImage ", img)
 }
-func (e *Echo) MsgVoice(voice *entry.VoiceRequest, back chan interface{}){
+func (e *Echo) MsgVoice(voice *entry.VoiceRequest, back chan interface{}) {
 	wechat.Info("Echo: MsgVoice ", voice)
 }
-func (e *Echo) MsgVideo(video *entry.VideoRequest, back chan interface{}){
+func (e *Echo) MsgVideo(video *entry.VideoRequest, back chan interface{}) {
 	wechat.Info("Echo: MsgVideo ", video)
 }
-func (e *Echo) MsgLink(link *entry.LinkRequest, back chan interface{}){
+func (e *Echo) MsgLink(link *entry.LinkRequest, back chan interface{}) {
 	wechat.Info("Echo: MsgLink ", link)
 }
-func (e *Echo) Location(location *entry.LocationRequest, back chan interface{}){
+func (e *Echo) Location(location *entry.LocationRequest, back chan interface{}) {
 	wechat.Info("Echo: Location ", location)
 }
 
-func (e *Echo) EventSubscribe(appoid string, oid string, back chan interface{}){
+func (e *Echo) EventSubscribe(appoid string, oid string, back chan interface{}) {
 	wechat.Info("Echo: EventSubscribe ", oid)
 	var subscriber entry.Subscriber
 	if err := e.Api.GetSubscriber(oid, &subscriber); err != nil {
@@ -41,34 +42,33 @@ func (e *Echo) EventSubscribe(appoid string, oid string, back chan interface{}){
 
 	back <- response
 }
-func (e *Echo) EventUnsubscribe(appoid string, oid string, back chan interface{}){
-	wechat.Info("Echo: EventUnsubscribe ", oid)	
+func (e *Echo) EventUnsubscribe(appoid string, oid string, back chan interface{}) {
+	wechat.Info("Echo: EventUnsubscribe ", oid)
 
 }
-func (e *Echo) EventMenu(appoid string, oid string, key string, back chan interface{}){
-	wechat.Info("Echo: EventMenu ", oid, key)	
+func (e *Echo) EventMenu(appoid string, oid string, key string, back chan interface{}) {
+	wechat.Info("Echo: EventMenu ", oid, key)
 }
 
 func main() {
 	wechat.SetLogger("console", "")
 	app := wechat.NewWeChatApp()
-	app.SetConfig("ini", "demo.ini")	
+	app.SetConfig("ini", "demo.ini")
 	app.SetCallback(NewEcho("demo"))
-
+	app.SetConfig("ini", "demo.ini")
 	//! 添加菜单
 	menu := entry.NewMenu()
-	btn1 := entry.NewViewButton("新浪","http://sina.com")
-	btn2 := entry.NewClickButton("点击","EVENT_MENU_CLICK")
+	btn1 := entry.NewViewButton("新浪", "http://sina.com")
+	btn2 := entry.NewClickButton("点击", "EVENT_MENU_CLICK")
 	btn3 := entry.NewButton("更多")
-	btn3.Append(entry.NewViewButton("腾讯","http://qq.com"))
-	btn3.Append(entry.NewViewButton("百度","http://baidu.com"))
-	btn3.Append(entry.NewViewButton("google","http://google.com"))
+	btn3.Append(entry.NewViewButton("腾讯", "http://qq.com"))
+	btn3.Append(entry.NewViewButton("百度", "http://baidu.com"))
+	btn3.Append(entry.NewViewButton("google", "http://google.com"))
 	menu.Add(btn1)
 	menu.Add(btn2)
 	menu.Add(btn3)
 
 	app.SetMenu(menu)
-
 
 	app.Run()
 }
